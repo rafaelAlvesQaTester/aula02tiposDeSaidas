f21a24ac97e62050052c8a6db1bf4b3ed1fccb3a
Rafael Alves <168144782+rafaelAlvesQaTester@users.noreply.github.com>
2025-07-20 16:46:16 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("textoQualquer").innerHTML="Meu primeiro texto <b> JavaScript...</b>"
+//alert("Esse é o alert...");
+console.log("Este é o console.log é mais discreto que o alert...");
